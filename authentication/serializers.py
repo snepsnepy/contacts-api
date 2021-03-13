92cb4aadd7a75f7cfa0ef7ff96bfee094fb70ea3
unknown <snep_graffiti20@yahoo.com>
2021-03-13 16:04:27 +0200

    First commit

---
+++ authentication/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=60,
+                                     min_length=8,
+                                     write_only=True)
+    email = serializers.EmailField(max_length=255, min_length=5)
+    first_name = serializers.CharField(max_length=60, min_length=2)
+    last_name = serializers.CharField(max_length=60, min_length=2)
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password']
+
+    #check if the email already exists in the DB
+    def validate(self, attrs):
+        email = attrs.get('email', '')
+        if User.objects.filter(email=email).exists():
+            raise serializers.ValidationError(
+                {'email': 'Email is already in use!'})
+        return super().validate(attrs)
+
+    #This 'create' creates the instance
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
---
+++ authentication/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from django.conf import settings
+from django.contrib import auth
+import jwt
+
+
+# Create your views here.
+class RegisterView(GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#
+class LoginView(GenericAPIView):
+    def post(self, request):
+        data = request.data
+        username = data.get('username', '')
+        password = data.get('password', '')
+
+        #When we have the username/pass
+        user = auth.authenticate(username=username, password=password)
+        #Generate a token for user
+        if user:
+            auth_token = jwt.encode({'username': user.username},
+                                    settings.JWT_SECRET_KEY,
+                                    algorithm="HS256")
+            print("Token: ", auth_token)
+
+            serializer = UserSerializer(user)
+
+            data = {'user': serializer.data, 'token': auth_token}
+
+            return Response(data, status=status.HTTP_200_OK)
+
+        #Send RES
+        return Response({'detail': 'Invalid credentials!'},
+                        status=status.HTTP_401_UNAUTHORIZED)
